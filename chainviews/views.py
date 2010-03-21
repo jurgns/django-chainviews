afdd1cb268bc6e124488bfbdc7a56040aab0e867
corey jergensen <corey@whitespacecoding.com>
2010-03-21 16:32:26 -0500

    ``get_object`` now accepts query keys as a dictionary thus removing the need for ``get_inline_object``. ``get_list`` has been renamed ``get_qs``. ``get_qs`` now accepts query keys as a dictionary, the same as ``get_object``, removing the need for an inline version as well.

--- chainviews/views.py
+++ chainviews/views.py
@@ -21,51 +21,29 @@ def if_(condition, action, else_action=None):
             return else_action(request, c)
         return request, c
 
-def get_object(Model, request_key='id', model_pk='pk', template_object_name='obj'):
+def get_object(Model, query_keys={'id': 'pk'}, template_object_name='obj'):
     """
     Retrieves and object from the database and places it in the context.
 
     ``Model`` can be a django Model, Manager or QuerySet.
     
-    The object will be retrieved based upon the model key provided in ``model_pk`` and the request 
-    variable ``request_key``. The resulting object will be placed in the context variable 
-    ``template_object_name``. An object not found based on criteria will result in server 404.
+    The object will be retrieved based upon the key-value pairs provided in ``query_keys``. The 
+    resulting object will be placed in the context variable ``template_object_name``. An object 
+    not found based on criteria will result in server 404.
     """
     def _get_object(request, c):
+        # function to fetch request key from context
+        _fetch = lambda kvpair: (kvpair[0], c[kvpair[1]])
+        # qs via django private method
         qs = _get_queryset(Model)
-        d = {model_pk: c[request_key]}
+        # map _fetch onto query_keys and return to dictionary
+        d = dict(map(_fetch, query_keys.items()))
+        # get or 404
         c[template_object_name] = get_object_or_404(qs, **d)
         return request, c
     return _get_object
 
-def get_inline_object(Model, parent_models, request_key='id', parent_request_keys=[], model_pk='pk', template_object_name='obj'):
-    """
-    Retrieves and object from the database and places it in the context.
-
-    ``Model`` can be a django Model, Manager or QuerySet.
-    ``parent_models`` is an ordered iterable of the parents.
-    ``parent_request_keys`` is a list of keys that will be used to pull the keys of the parents 
-    from context. If it is not provided, it will constructed based upon the models given in 
-    ``parent_models``.
-    
-    The object will be retrieved based upon the model key provided in ``model_pk`` and the request 
-    variable ``request_key``. The resulting object will be placed in the context variable 
-    ``template_object_name``. An object not found based on criteria will result in server 404.
-    """
-    # construct parent request keys if needed
-    parent_request_keys = parent_request_keys or ["%s_id" % parent._meta.object_name.lower() for parent in parent_models]
-    # construct query keys for reference to parents
-    query_keys = _get_parent_query_keys([parent._meta.object_name.lower() for parent in parent_models])
-    #
-    def _get_object(request, c):
-        qs = _get_queryset(Model)
-        d = dict([(query_key, c[key]) for query_key, key in zip(query_keys, parent_request_keys)])
-        d[model_pk] = c[request_key]
-        c[template_object_name] = get_object_or_404(qs, **d)
-        return request, c
-    return _get_object
-
-def get_list(Model, template_qs_name='qs'):
+def get_qs(Model, query_keys={}, template_qs_name='qs'):
     """
     Retrieves a list for ``Model`` and places it in the context.
 
@@ -73,35 +51,20 @@ def get_list(Model, template_qs_name='qs'):
 
     The object list retrived will be placed in the context variable ``template_qs_name``.
     """
-    def _get_list(request, c):
+    def _get_qs(request, c):
         qs = _get_queryset(Model)
-        c[template_qs_name] = qs
+        if query_keys:
+            # function to fetch request key from context
+            _fetch = lambda kvpair: (kvpair[0], c[kvpair[1]])
+            # map _fetch onto query_keys and return to dictionary
+            keys = map(_fetch, query_keys.items())
+            keys = dict(keys)
+            # filter
+            c[template_qs_name] = qs.filter(**keys)
+        else:
+            c[template_qs_name] = qs
         return request, c
-    return _get_list
-
-def get_inline_list(Model, parent_models, parent_request_keys=[], template_qs_name='qs'):
-    """
-    Retrieves a list for ``Model`` and places it in the context.
-
-    ``Model`` can be a django Model, Manager or QuerySet.
-    ``parent_models`` is an ordered iterable of the parents.
-    ``parent_request_keys`` is a list of keys that will be used to pull the keys of the parents 
-    from context. If it is not provided, it will constructed based upon the models given in 
-    ``parent_models``.
-
-    The object list retrived will be placed in the context variable ``template_qs_name``.
-    """
-    # construct parent request keys if needed
-    parent_request_keys = parent_request_keys or ["%s_id" % parent._meta.object_name.lower() for parent in parent_models]
-    # construct query keys for reference to parents
-    query_keys = _get_parent_query_keys([parent._meta.object_name.lower() for parent in parent_models])
-    #
-    def _get_list(request, c):
-        qs = _get_queryset(Model)
-        d = dict([(query_key, c[key]) for query_key, key in zip(query_keys, parent_request_keys)])
-        c[template_qs_name] = qs.filter(**d)
-        return request, c
-    return _get_list
+    return _get_qs
 
 def edit(Form, template_form_name='edit_form', template_obj_name='obj', method='POST'):
     """
